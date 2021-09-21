b57ede17a482996baa3618ba90d5a81ac0ab7ed8
Anton Krishtof <a.krischtoff@gmail.com>
2021-09-22 00:11:17 +0300

    clean up modue 4 solution

--- module4-solution/src/menu/categories.controller.js
+++ module4-solution/src/menu/categories.controller.js
@@ -6,7 +6,6 @@ CategoriesListController.$inject = ['MenuDataService', 'items'];
 function CategoriesListController(MenuDataService, items) {
   var categoriesList = this;
   categoriesList.items = items;
-  // console.log(categoriesList.items)
 }
 
 })();
